258adf6d5930bda3d394a0117620266b91444ec8
Peter Pisarz <peter.pisarz@gmail.com>
2022-07-21 20:38:30 -0500

    Twitter > Hollystcrypto

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,7 @@ const Social = () => {
             <h2>Socials</h2>
 
             <a href="peter.pisarz@gmail.com" className="button">peter.pisarz@gmail.com</a>
-            <a href="https://twitter.com/peterhollyatx" target="_blank" className="button">Twitter</a>
+            <a href="https://twitter.com/hollystcrypto" target="_blank" className="button">Twitter</a>
             <a href="https://www.linkedin.com/in/peter-pisarz-6b339088/" target="_blank" className="button">LinkedIn</a>
             <a href="https://github.com/peterpisarz" target="_blank" className="button">GitHub</a>
         </div>
