6d45473a62513d6074a520625708248cfd621bf2
peterpisarz <peter.pisarz@gmail.com>
2024-07-07 22:40:13 -0500

    Updating About

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,14 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
+                As a former aerospace engineer, I found my career disrupted during the pandemic.
+                With commercial air travel down over 90%, I knew it was time to 
+                learn new skills. I had purchased some of my first cryptocurrencies but admitted to
+                myself that I really didn't understand how it worked, so I began to dive deeper into
+                the programming side of things.
+
+                <br/><br/>
+                
                 I started with Dapp University in Spring of 2021 with their Blockchain Bootcamp. The bootcamp walks you 
                 through creating a decentralized token exchange (DEX) you'll find in my portfolio below. I also took 
                 an interest in NFTs and created a project from some of my own photography. 
@@ -19,7 +27,7 @@ const About = () => {
                 
                 <br/><br/> 
                 
-                Most recently I have been spending a majority of my time in DeFi. I have developed a basic arbitrage 
+                I have been spending a majority of my time lately in DeFi. I have developed a basic arbitrage 
                 trading bot which balances prices across DEXs and makes a profit using flashloans. I have expanded this bot to work
                 on many different chains and interfaces. Currently I am expanding the bot onto new DEXs like Aerodrome on the Base L2 chain. 
                 
